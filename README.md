11164cf0858f97c1db6c7eff90502d903e2c5ca3
Johann Barnard <johann.b@telus.net>
2019-05-07 23:37:43 -0700

    Build a minimal kernal, with support for running in QEMU

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
 # `rust_os`
 My implementation of [this **amazing** guide](https://os.phil-opp.com/).
 
-## Building
+## Dependencies
+
 ```shell
-$ rustup target add thumbv7em-none-eabihf
-$ cargo build --target thumbv7em-none-eabihf
+$ rustup component add rust-src
+$ rustup component add llvm-tools-preview
+$ cargo install cargo-xbuild
+$ cargo install bootimage --version "^0.7.3"
+```
+
+##  Building
+To build a standalone executable:
+
+```shell
+$ cargo xbuild
+```
+
+To build a bootable disk image:
+
+```shell
+$ cargo bootimage
+```
+
+## Booting in QEMU
+First, install QEMU. Next, run:
+
+```shell
+$ cargo xrun
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-#![no_std]
-#![no_main]
+#![no_std]  // Don't link the Rust standard library.
+#![no_main] // Disable all Rust-level entry points.
 
 use core::panic::PanicInfo;
 
@@ -9,7 +9,20 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
+/// This function is the entry point, since the linker looks for a function named
+/// `_start` by default. Currently, it prints "Hello World!" to the VGA buffer.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
