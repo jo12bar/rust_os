a02295aa60a8cad56e17b356051133f4911ec71f
Johann Barnard <johann.b@telus.net>
2019-05-12 11:18:31 -0700

    Write a basic VGA driver, with support for formatting macros

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,23 @@
 #![no_std]  // Don't link the Rust standard library.
 #![no_main] // Disable all Rust-level entry points.
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
-
-static HELLO: &[u8] = b"Hello World!";
 
 /// This function is the entry point, since the linker looks for a function named
 /// `_start` by default. Currently, it prints "Hello World!" to the VGA buffer.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
 
     loop {}
 }
