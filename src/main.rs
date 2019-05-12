8a143af2fba14e83f6ced718743f5b95ad8196ea
Johann Barnard <johann.b@telus.net>
2019-05-12 12:42:52 -0700

    Add basic support for integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![no_std]  // Don't link the Rust standard library.
 #![no_main] // Disable all Rust-level entry points.
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 mod vga_buffer;
 
 use core::panic::PanicInfo;
@@ -17,7 +21,43 @@ fn panic(info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
+}
+
+/// Possible exit codes for QEMU. Useful for debugging, testing, etc.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed =  0x11,
+}
+
+/// Exit from QEMU with some `QemuExitCode`. Useful for debugging, testing, etc.
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
