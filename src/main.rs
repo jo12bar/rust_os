058736db5b4a8f73a29df6b9a345232c14073542
Johann Barnard <johann.b@telus.net>
2019-05-12 23:56:49 -0700

    Extract lib and add integration tests

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,11 @@
 #![no_main] // Disable all Rust-level entry points.
 
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod serial;
-mod vga_buffer;
-
 use core::panic::PanicInfo;
-
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
+use rust_os::println;
 
 /// This function is the entry point, since the linker looks for a function named
 /// `_start` by default. Currently, it prints "Hello World!" to the VGA buffer.
@@ -30,38 +20,16 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-/// Possible exit codes for QEMU. Useful for debugging, testing, etc.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed =  0x11,
-}
-
-/// Exit from QEMU with some `QemuExitCode`. Useful for debugging, testing, etc.
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-    exit_qemu(QemuExitCode::Success);
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
 }
 
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
+    rust_os::test_panic_handler(info)
 }
