949f26d43b82cc4936c009bda88028679e409dba
Johann Barnard <johann.b@telus.net>
2019-05-20 00:09:12 -0700

    Implement virtual-physical memory mapping
    ...using 4-level page tables.

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,14 @@
 
 pub mod gdt;
 pub mod interrupts;
+pub mod memory;
 pub mod serial;
 pub mod vga_buffer;
 
 use core::panic::PanicInfo;
+
+#[cfg(test)]
+use bootloader::{BootInfo, entry_point};
 
 /// Initialize everything.
 pub fn init() {
@@ -43,10 +47,12 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     hlt_loop();
 }
 
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
 /// Entry point for `cargo xtest`
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
     hlt_loop();
--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,40 @@
 #![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{BootInfo, entry_point};
 use core::panic::PanicInfo;
 use rust_os::println;
 
+entry_point!(kernel_main);
+
 /// This function is the entry point, since the linker looks for a function named
 /// `_start` by default. Currently, it prints "Hello World!" to the VGA buffer.
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use rust_os::memory;
+    use x86_64::{structures::paging::MapperAllSizes, VirtAddr};
+
     println!("Hello World{}", "!");
 
     rust_os::init();
+
+    let mapper = unsafe { memory::init(boot_info.physical_memory_offset) };
+
+    let addresses = [
+        // The identity-mapped vga buffer page
+        0xb8000,
+        // Some code page
+        0x20010a,
+        // Some stack page
+        0x57ac_001f_fe48,
+        // Virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = mapper.translate_addr(virt);
+        println!("{:?} -> {:?}", virt, phys);
+    }
 
     #[cfg(test)]
     test_main();
