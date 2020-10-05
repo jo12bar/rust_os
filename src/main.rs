bcc635b13b3f19d8472d297fc5bd4215ae77a3b6
Johann Barnard <johann.b@telus.net>
2020-10-04 22:33:43 -0600

    Set up a minimal rust kernel, running in QEMU

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-#![no_std]
-#![no_main]
+#![no_std] // Don't link the Rust standard library
+#![no_main] // Disable all Rust-level entry points
 
 use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
 
 /// This function is called on panic.
 #[panic_handler]
@@ -9,7 +11,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+/// Main kernel entry point. Uses the Linux convention of being called `_start`.
+/// That also means that we have to avoid mangling the function's name.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
