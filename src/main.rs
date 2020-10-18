ae49916f6c8779b41e444dc0e642645f6f90c0f5
Johann Barnard <johann.b@telus.net>
2020-10-17 19:52:43 -0600

    Translate virtual addresses to physical addresses

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,7 @@ entry_point!(kernel_main);
 /// Main kernel entry point. Uses the Linux convention of being called `_start`.
 /// That also means that we have to avoid mangling the function's name.
 pub fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use rust_os::memory::active_level_4_table;
-    use x86_64::structures::paging::PageTable;
+    use rust_os::memory::translate_addr;
     use x86_64::VirtAddr;
 
     println!("Hello World!");
@@ -25,26 +24,22 @@ pub fn kernel_main(boot_info: &'static BootInfo) -> ! {
     rust_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
 
-    // Print non-empty entries of the level 4 table:
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
+    let addresses = [
+        // The identity-mapped vga buffer page
+        0xb8000,
+        // Some code page
+        0x201008,
+        // Some stack page
+        0x0100_0020_1a10,
+        // Virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
 
-            // Get the physical address from the entry and convert it
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe { &*ptr };
-
-            // Print non-empty entries of the level 3 table:
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("  L3 Entry {}: {:?}", i, entry);
-                }
-            }
-        }
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
     }
 
     // If this kernel was started via `cargo test`, then run all the tests.
